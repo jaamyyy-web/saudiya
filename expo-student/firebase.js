1da6e285065acdb3dee1366a7ac646821dd92dc8
jaamyyy-web <jaamyyy@gmail.com>
2026-05-23 17:22:46 +0530

    Export Firebase Auth for student app

--- expo-student/firebase.js
+++ expo-student/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBscnKyw3dldC7UudkzDd3XNBWRFxr9ZM0',
@@ -11,4 +12,7 @@ const firebaseConfig = {
 };
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+export { app };
